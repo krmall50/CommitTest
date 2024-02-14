c5bbbff6b8ec03c65584773b72e27a2acebb411a
Ernar <230152@astanait.edu.kz>
2024-02-14 17:16:34 +0600

    Third commit with for loop

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Some update for second commit v2");
+        for (int i = 0; i<5; i++){
+            System.out.println(i);
+        }
     }
 }
